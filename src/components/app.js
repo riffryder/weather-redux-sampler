6755fb64aa135515c1302d34164957cda98f7c31
Raphael Croce <rcroce7@gmail.com>
2018-06-23 17:23:50 -0500

    Render search bar and weather list components inside of app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+import SearchBar from '../containers/SearchBar';
+import WeatherList from '../containers/WeatherList';
+
+class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+        <WeatherList />
+      </div>
     );
   }
 }
+
+export default App;
